9db235d2a92e6e2f987bb04a6817cc9aeab35c1b
ericjhlee <eric_lee93@hotmail.com>
2015-01-18 16:05:42 -0800

    platediv style_change

--- app/assets/javascripts/restaurant.js
+++ app/assets/javascripts/restaurant.js
@@ -1,28 +1,27 @@
-$(function () {
-    var body = $('.background');
-    var backgrounds = [
-      'url(/assets/food_2.jpg)', 
-      'url(/assets/rest_1.JPG)',
-      'url(/assets/rest_2.jpg)',
-      'url(/assets/rest_3.JPG'];
-    var current = 0;
+// $(function () {
+//     var body = $('.background');
+//     var backgrounds = [
+//       'url(/assets/food_2.jpg)', 
+//       'url(/assets/rest_1.JPG)',
+//       'url(/assets/rest_2.jpg)',
+//       'url(/assets/rest_3.JPG'];
+//     var current = 0;
 
-    function nextBackground() {
-        body.css({"background": backgrounds[current = ++current % backgrounds.length],
-        	"background-repeat":"no-repeat",
-	    	"background-size":"cover",
-	    	"background-position":"fixed",
-	    	"background-attachment":"fixed"});
+//     function nextBackground() {
+//         body.css({"background": backgrounds[current = ++current % backgrounds.length],
+//         	"background-repeat":"no-repeat",
+// 	    	"background-size":"cover",
+// 	    	"background-position":"fixed",
+// 	    	"background-attachment":"fixed"});
 
-        setTimeout(nextBackground, 5000);
-    }
-    setTimeout(nextBackground, 5000);
-    body.css("background",backgrounds[0]);
-});
+//         setTimeout(nextBackground, 5000);
+//     }
+//     setTimeout(nextBackground, 5000);
+//     body.css("background",backgrounds[0]);
+// });
 
-
-// body.css({"background":backgrounds[0], 
-//     	"background-repeat":"no-repeat",
-//     	"background-size":"cover",
-//     	"background-position":"fixed",
-//     	"background-attachment":"fixed"})
+// // body.css({"background":backgrounds[0], 
+// //     	"background-repeat":"no-repeat",
+// //     	"background-size":"cover",
+// //     	"background-position":"fixed",
+// //     	"background-attachment":"fixed"})
