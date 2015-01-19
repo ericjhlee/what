1601a938983023772bf98ab19841cf109b25d3d8
dennisjs0910 <dennis.joonsuk.yi@gmail.com>
2015-01-18 18:05:26 -0800

    homepage text added + css

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,18 @@
 class HomepageController < ApplicationController
   def index
-  end
+  # 	if cookies[:lat_lng]
+  # 		redirect_to restaurant_index_url
+  # 	else
+  # 		find_cookie
+  # 	end
+  # end
+
+  # private
+
+  # def find_cookie
+  # 	if cookies[:lat_lng]
+  # 		redirect_to restaurant_index_url
+  # 	end
+  # end
+end
 end
